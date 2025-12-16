afabd985c881500e1ba4bfd87299e5134b25797a
Farah <ncirfarah7@gmail.com>
2025-12-16 23:51:39 +0100

    3rd commit

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataSchema");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
+app.get("/index.html", (req, res) => {
+  res.send("<h1>data saved successfully</h1>");
+});
 
 mongoose
   .connect(
-    "mongodb+srv://storing:uRXYPgYXRnnschQF@cluster0.ecioab9.mongodb.net/?appName=Cluster0"
+    "mongodb+srv://storing:uRXYPgYXRnnschQF@cluster0.ecioab9.mongodb.net/all-data?appName=Cluster0"
   )
   .then(() => {
     app.listen(port, () => {
@@ -20,3 +25,16 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const mydata = new Mydata(req.body);
+  mydata
+    .save()
+    .then((result) => {
+      res.redirect("/index.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
