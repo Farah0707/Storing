6484df89d4a369aa1afeca7b365362ca002cfb57
Farah <ncirfarah7@gmail.com>
2025-12-16 21:24:34 +0100

    connection with DB fixed

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/ is running`);
-});
+
+mongoose
+  .connect(
+    "mongodb+srv://storing:uRXYPgYXRnnschQF@cluster0.ecioab9.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/ is running`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
