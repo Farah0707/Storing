7efe7cb4f0ecd716c960b776b4e66b86148de51e
Farah <ncirfarah7@gmail.com>
2025-12-17 20:14:52 +0100

    delete fixed

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+var moment = require("moment");
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 //auto refresh
 const path = require("path");
@@ -27,7 +30,7 @@ liveReloadServer.server.once("connection", () => {
 app.get("/", (req, res) => {
   User.find()
     .then((result) => {
-      res.render("index", { arr: result });
+      res.render("index", { arr: result , moment: moment});
     })
     .catch((err) => {
       console.log(err);
@@ -35,15 +38,29 @@ app.get("/", (req, res) => {
 });
 
 app.get("/user/add.html", (req, res) => {
-  res.render("user/add");
+  res.render("user/add"); 
 });
 
-app.get("/user/view.html", (req, res) => {
-  res.render("user/view");
+app.get("/edit/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/edit", { obj: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get("/user/edit.html", (req, res) => {
-  res.render("user/edit");
+
+
+app.get("/view/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view", { obj: result , moment: moment});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //POST requests
@@ -58,6 +75,15 @@ app.post("/user/add.html", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+//Delete request
+app.delete("/edit/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id).then(() => {  
+    res.redirect("/");
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 
 mongoose
--- models/customerSchema.js
+++ models/customerSchema.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // define the Schema (the structure of the article)
-const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phoneNumber: Number,
-  age: Number,
-  country: String,
-  gender: String
-});
+const userSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phoneNumber: Number,
+    age: Number,
+    country: String,
+    gender: String,
+  },
+  { timestamps: true }
+);
 
 // Create a model based on that schema
 const User = mongoose.model("customer", userSchema);
